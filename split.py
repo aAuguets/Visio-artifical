bdb7671b7304fb8f66f7397977e761cc18c36280
Jordi Masip <jordi@masip.cat>
2013-11-28 17:16:43 +0100

    Funció definida

--- split.py
+++ split.py
@@ -2,3 +2,10 @@
 # -*- coding: utf-8 -*-
 
 # AUTOR: Jordi Masip
+
+def split digit(img):
+	"""
+	Aquesta funció rebrà una imatge img en blanc i negre retallada verticalment i retorna una tupla (D,R) en la que D és una
+	imatge amb el dígit de més a l’esquerra i R és la resta de la imatge. La imatge corresponent al dígit extret D es retorna
+	convenientment retallada en la direcció horitzontal. La resta R esdevé una imatge nul.la quan s’han extret tots els dígits.
+	"""
