f1f45d69a400847a2e975d7c92ee56882e571172
JMasip <jordi@masip.cat>
2013-12-09 20:30:29 +0100

    load_patterns() implementada

--- match.py
+++ match.py
@@ -2,3 +2,19 @@
 # -*- coding: utf-8 -*-
 
 # AUTOR: Jordi Masip
+
+from utiles import *
+from imgio import *
+
+def load_patterns(prefix):
+	patrons = []
+	for num in range(10):
+		name = prefix + "_" + num + ".jpeg"
+		debug("Carregant '" + name + "'")
+		patrons += [read_bn(name)]
+	return patrons
+
+def match(img, patlst):
+	for pattern in patlst:
+		pass
+	return []
---
+++ utiles.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# AUTOR: Jordi Masip
+
+DEBUG = True
+
+def debug(msg):
+	if DEBUG:
+		print "[DEBUG]", msg
