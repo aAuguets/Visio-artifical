e20ab51ad89b2411779c1ffdb34609f9e637050d
Jordi Masip <JMasip@users.noreply.github.com>
2013-12-19 18:53:28 +0100

    Update split.py

--- split.py
+++ split.py
@@ -3,28 +3,34 @@
 
 # AUTOR: Jordi Masip
 
-def split digit(img):
+def split_digit(img):
 	"""
 	Aquesta funció rebrà una imatge img en blanc i negre retallada verticalment i retorna una tupla (D,R) en la que D és una
 	imatge amb el dígit de més a l’esquerra i R és la resta de la imatge. La imatge corresponent al dígit extret D es retorna
 	convenientment retallada en la direcció horitzontal. La resta R esdevé una imatge nul.la quan s’han extret tots els dígits.
 	"""
-	BLACK, WHITE = 0, 255
-	n_rows = len(img)
+	
 	column_pos = -1
-	i, j = 0, 0
-	nova_imatge = []
-	img_restant = []
+	i = 0
 	for row in img:
 		i += 1
 		is_vertical = True
 		for value in img[column][row]:
-			j += 1
-			if value == BLACK:
+			if value == 0:
 				is_vertical *= False
-		column_pos = i
+		else:
+			column_pos = i
+			break
+	
+	print "La primera columna vertical està a la pos:", columns_pos
+	
+	# Si no s'ha trobat cap columna vertical "blanca"...
+	if column_pos == -1:
+		return (img, [])
 		
 	i, j = 0, 0
+	nova_imatge = []
+	img_restant = []
 	for row in img:
 		i += 1
 		for bit in img[row]:
