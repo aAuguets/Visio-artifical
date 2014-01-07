7baa96c6e3079d78db38b6335362d361ff0725f5
JMasip <jordi@masip.cat>
2014-01-07 19:04:21 +0100

    Canvis que va fer el Felipe

--- img.py
+++ img.py
@@ -1,69 +1,74 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from PIL import Image,ImageColor, ImageDraw
+from PIL import Image, ImageColor, ImageDraw
 from imgio import *
-
-
-im = Image.open("test2.jpeg")
 
 def format(im):
 	"""
 	Donada imatge retorna format: RGB, 1 (blanc i negre), L (escala de grisos)
 	"""
-	return im.mode
+	return im[0]
 
 def get_w(im):
 	"""
 	Donada imatge retorna l'amplada
 	"""
-	return im.size[0]
-
+	return len(im[1][0])
 def get_h(im):
 	"""
 	Donada imatge retorna l'alcada
 	"""
-	return im.size[1]
-	
+	return len(im[1])
 def white_rgb(w, h):
 	"""
 	Retorna image en format RGB amb tamany w, h de color blanc
 	"""
-	return Image.new("RGB", (w, h), (255, 255, 255))
-		
-#ima=Image.open("matricula1.jpeg")
-#print get_h(ima)
+	fila = [(255, 255, 255) for i in range(w)]
+	img = [fila for i in range(h)]
+	return ("RGB", img)
 
 def white_grey(w, h):
-	return Image.new("L", (w, h), (255))
+	fila = [255 for i in range(w)]
+	img = [fila for i in range(h)]
+	return ("L", img)
 
 def white_bn(w, h):
-	return Image.new("1", (w, h), (255))
+	fila = [255 for i in range(w)]
+	img = [fila for i in range(h)]
+	return ("1", img)
 	
 def matrix(img):
-	pix = img.load()
-	X, Y = img.size
-	data = [[pix[x,y] for x in range(X)] for y in range(Y)]
-	#nova = []    
-	#for row in data:
-	#	r = []
-	#	for bit in row:
-	#		if bit > 125:
-	#			r+=[255]
-	#		else:
-	#			r+=[0]
-	#	nova+=[r]
-	return data
-    
-def subimg(img, ow, oh, w, h):
-	img.crop()
+	return img[1]
 	
-#m=white_grey(421, 420)
-#print matrix(white_bn(400, 400))
+def subimg(img, ow, oh, w, h):
+	"""
+	subimg(image,column,row,widthColumns,heightRows)
+	Returns a subimage from img with origin coordinates ow,oh and size w x h
+	>>> subimg(('L', [[0, 0, 255], [255, 255, 255], [255, 255, 255]]),0,0,2,1)
+	('L', [[0, 0]])
+	>>> subimg(('1', [[255, 255], [255, 255], [255, 255]]),0,1,2,1)
+	('1', [[255, 255]])
+	>>> subimg(('1', [[255, 255], [255, 255], [0, 255]]),0,1,2,2)
+	('1', [[255, 255], [0, 255]])
+	"""
+	imatge=img[1]
+	imatge=imatge[oh:h+1][ow:w+1]
+	print imatge
 
-#im.show()
-#print "Alcada: ",get_h(m)," ","Amplada: ",get_w(m)," ","
-#print "Format: ",format(im)
-#m.show()
-
-
+def img(m, model='DISCOVER'):
+	"""
+	Returns the image representation format (T,m)
+	>>> img([[255,255,0],[255,128,255],[191,255,255]],’DISCOVER’)
+	(’L’, [[255, 255, 0], [255, 128, 255], [191, 255, 255]])
+	>>> img([[255,255,0],[255,0,255],[0,255,255]],’DISCOVER’)
+	(’1’, [[255, 255, 0], [255, 0, 255], [0, 255, 255]])
+	"""
+	if isinstance(m[0][0], tuple): # RGB
+			return ("RGB", m)
+	for a in m: #NO RGB
+		for b in a:
+			if b != 255 and b != 0: 
+				return ("L", m)
+			else:
+				return ("1", m)
