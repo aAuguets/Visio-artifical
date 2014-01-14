8dfd7b8e871f69d9d040a88484ecc3dbcf336cc5
JMasip <jordi@masip.cat>
2014-01-14 19:52:53 +0100

    Noves funcions: getFirstRowOfColor(), getFirstColumnOfColor(), transpose()

--- split.py
+++ split.py
@@ -1,9 +1,40 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# AUTOR: Jordi Masip
+# DEV: Jordi Masip
 
 from utiles import *
+
+def transpose(img):
+	"""
+	Retorna una matriu amb les files com a columnes i les columnes com a files
+	"""
+	# Crea tantes files a transposed_img com columnes hi ha a img:
+	transposed_img = [[] for x in range(len(img[0]))]
+
+	for row in img:
+		i = 0
+		for column in row:
+			transposed_img[i] += [column]
+			i += 1
+
+	return transposed_img
+
+def getFirstRowOfColor(color, img):
+	"""
+	Retorna la posició de la última columna de la imatge que és tota blanca abans de trobar una negra (o arribar al final)
+	"""
+	i, position = 0, -1
+	for this in img:
+		for pixel in this:
+			if pixel == color:
+				position = i + 1
+				break
+		i += 1
+	return position
+
+def getFirstColumnOfColor(color, img):
+	return getFirstRowOfColor(color, transpose(img))
 
 def split_digit(img):
 	"""
@@ -11,28 +42,7 @@ def split_digit(img):
 	imatge amb el dígit de més a l’esquerra i R és la resta de la imatge. La imatge corresponent al dígit extret D es retorna
 	convenientment retallada en la direcció horitzontal. La resta R esdevé una imatge nul.la quan s’han extret tots els dígits.
 	"""
-	end_white = -1
-	i = 0
-	j = 0
-	for column in zip(img):
-		i += 1
-		is_vertical = True
-		# Per cada element de la columna...
-		for value in column:
-			print i,":",j
-			j+=1
-			# Si troba un caràcter zero, salta a la següent columna
-			if value == BLACK:
-				print "Black!!!"
-				is_vertical = False
-				break
-	else:
-		if end_white == -1:
-			debug("White -1")
-			end_white = i
-	
-	debug("La primera columna de color blanc està a la pos: " + str(end_white))
-	
+	debug("La última columna tota blanca està a la pos: " + str(end_white))
 	# Si no s'ha trobat cap columna vertical "blanca"...
 	if end_white == -1:
 		return (img, [])
@@ -46,3 +56,7 @@ def split_digit(img):
 			img_char += [row[:end_white]]
 		i += 1
 	return (img_char, img_restant)
+
+split_digit([[255,255,255, 0],[255,255,255, 255]])
+#print [[255,255,255, 0],[255,255,255, 255]]
+#print transpose([[255,255,255, 0],[255,255,255, 255]])
